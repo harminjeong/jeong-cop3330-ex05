13179ade83feceefc4d7ab49400e892100b71761
Harmin Jeong <harminjeong@gmail.com>
2021-09-13 20:47:34 -0400

    initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,23 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Harmin Jeong
+ */
+package org.example;
+import java.util.Scanner;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is the first number? ");
+        int num1 = input.nextInt();
+        System.out.print("What is the second number? ");
+        int num2 = input.nextInt();
+
+        System.out.println(num1+" + "+num2+" = "+(num1+num2));
+        System.out.println(num1+" - "+num2+" = "+(num1-num2));
+        System.out.println(num1+" * "+num2+" = "+(num1*num2));
+        System.out.println(num1+" / "+num2+" = "+(num1/num2));
+    }
+}
